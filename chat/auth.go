051ab1835dadb0014123e8b356101c712dde9a84
Lua00808 <3102oikawa713hayaki@gmail.com>
2019-11-24 00:08:44 +0900

    ログイン時に認証用のページにリダイレクトさせる(とりあえず google だけ)

--- chat/auth.go
+++ chat/auth.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"strings"
+
+	"github.com/stretchr/gomniauth"
 )
 
 type authHandler struct {
@@ -24,3 +29,29 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func MustAuth(handler http.Handler) http.Handler {
 	return &authHandler{next: handler}
 }
+
+// loginHandler はサードパーティーへのログインの処理を待ち受ける。
+// パスの形状: /suth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	action := segs[2]   // TODO: 適切でないリクエストを受け取った場合に適切なエラーメッセージを出力するようにハンドリングする。
+	provider := segs[3] // 〃
+	switch action {
+	case "login":
+		log.Println("TODO: ログイン処理", provider)
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			log.Fatalln("認証プロバイダーの取得に失敗しました: ", provider, "-", err)
+		}
+		loginURL, err := provider.GetBeginAuthURL(nil, nil)
+		if err != nil {
+			log.Fatalln("GetBeginAuthURLの呼び出し中にエラーが発生しました: ", provider, "-", err)
+		}
+		w.Header().Set("Location", loginURL)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "アクション%sには非対応です", action)
+	}
+}
+
--- chat/main.go
+++ chat/main.go
@@ -9,7 +9,12 @@ import (
 	"path/filepath"
 	"sync"
 
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+
 	"github.com/golang-chatapp/trace"
+	"github.com/stretchr/gomniauth"
 )
 
 // templ は1つのテンプレートを表す
@@ -35,10 +40,18 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse() //フラグを解釈する
+	// Gomniauth のセットアップ
+	gomniauth.SetSecurityKey("CUgAcQhgAO4o30ePr55MGUkwM7Ur85EbCu3nlPdizhLBMCMq6FcihtVR7kxOrlkl")
+	gomniauth.WithProviders(
+		facebook.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/facebook"),
+		github.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/github"),
+		google.New("31954288968-sfo7524s135kpljpkq7sh4kmr6n3mfs5.apps.googleusercontent.com", "oxkcfwxPNhgyc78jA6zoLsa2", "http://localhost:8080/auth/callback/google"),
+	)
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	//チャットルームを開始
 	go r.run()
