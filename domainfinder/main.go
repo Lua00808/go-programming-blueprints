7d6f9d950d961b900117a9d6b9e209ff3e964f2c
Lua00808 <3102oikawa713hayaki@gmail.com>
2020-06-29 22:40:31 +0900

    Update main.go

--- domainfinder/main.go
+++ domainfinder/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "os/exec"
+import (
+	"log"
+	"os"
+	"os/exec"
+)
 
 var cmdChain = []*exec.Cmd{
 	exec.Command("lib/synonyms"),
@@ -11,5 +15,28 @@ var cmdChain = []*exec.Cmd{
 }
 
 func main() {
+	cmdChain[0].Stdin = os.Stdin
+	cmdChain[len(cmdChain)-1].Stdout = os.Stdout
 
+	for i := 0; i < len(cmdChain)-1; i++ {
+		thisCmd := cmdChain[i]
+		nextCmd := cmdChain[i+1]
+		stdout, err := thisCmd.StdoutPipe()
+		if err != nil {
+			log.Panicln(err)
+		}
+		nextCmd.Stdin = stdout
+	}
+	for _, cmd := range cmdChain {
+		if err := cmd.Start(); err != nil {
+			log.Panicln(err)
+		} else {
+			defer cmd.Process.Kill()
+		}
+	}
+	for _, cmd := range cmdChain {
+		if err := cmd.Wait(); err != nil {
+			log.Panicln(err)
+		}
+	}
 }
