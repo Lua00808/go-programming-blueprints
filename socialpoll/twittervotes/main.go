fb98cbb7e8cf1c5e5deba839495d8c52e430a92e
Lua00808 <3102oikawa713hayaki@gmail.com>
2020-08-30 22:56:01 +0900

    Add func publishVotes

--- socialpoll/twittervotes/main.go
+++ socialpoll/twittervotes/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/nsqio/go-nsq"
 	"gopkg.in/mgo.v2"
 )
 
@@ -32,4 +33,21 @@ func loadOptions() ([]string, error) {
 	}
 	iter.Close()
 	return options, iter.Err()
+}
+
+func publishVotes(votes <-chan string) <-chan struct{} {
+	stopchan := make(chan struct{}, 1)
+	pub, _ := nsq.NewProducer("localhost:4150", nsq.NewConfig())
+	go func() {
+		defer func() {
+			stopchan <- struct{}{}
+		}()
+		for vote := range votes {
+			pub.Publish("votes", []byte(vote)) // 投票内容をパブリッシュします
+		}
+		log.Println("Publisher: 停止中です")
+		pub.Stop()
+		log.Println("Publisher: 停止しました")
+	}()
+	return stopchan
 }
