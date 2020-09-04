6fe65c190c5781740a9741b3a53604975046fb04
Lua00808 <3102oikawa713hayaki@gmail.com>
2020-09-05 03:20:27 +0900

    Add a function to push latest data to DB

--- socialpoll/counter/main.go
+++ socialpoll/counter/main.go
@@ -5,10 +5,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	"sync"
+	"syscall"
+	"time"
 
 	"github.com/nsqio/go-nsq"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var fatalErr error
@@ -18,6 +22,9 @@ func fatal(e error) {
 	flag.PrintDefaults()
 	fatalErr = e
 }
+
+const updateDuration = 1 * time.Second
+
 func main() {
 	defer func() {
 		if fatalErr != nil {
@@ -59,5 +66,47 @@ func main() {
 	if err := q.ConnectToNSQLookupd("localhost:4161"); err != nil {
 		fatal(err)
 		return
+	}
+
+	log.Println("NSQ 上での投票を待機します...")
+	var updater *time.Timer
+	updater = time.AfterFunc(updateDuration, func() {
+		countsLock.Lock()
+		defer countsLock.Unlock()
+		if len(counts) == 0 {
+			log.Println("新しい投票はありません。データベースの更新をスキップします")
+		} else {
+			log.Println("データベースを更新します...")
+			log.Println(counts)
+			ok := true
+			for option, count := range counts {
+				sel := bson.M{"options": bson.M{"$in": []string{option}}}
+				up := bson.M{"$inc": bson.M{"results." + option: count}}
+				if _, err := pollData.UpdateAll(sel, up); err != nil {
+					log.Println("更新に失敗しました:", err)
+					ok = false
+					continue
+				}
+				counts[option] = 0
+			}
+			if ok {
+				log.Println("データベースの更新が完了しました")
+				counts = nil // 	得票数をリセットします
+			}
+		}
+		updater.Reset(updateDuration)
+	})
+
+	termChan := make(chan os.Signal, 1)
+	signal.Notify(termChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	for {
+		select {
+		case <-termChan:
+			updater.Stop()
+			q.Stop()
+		case <-q.StopChan:
+			// 完了しました
+			return
+		}
 	}
 }
