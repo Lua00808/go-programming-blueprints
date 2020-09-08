e871eba8dfca982e99e40c25f848fb71acab9e66
Lua00808 <3102oikawa713hayaki@gmail.com>
2020-09-08 23:01:39 +0900

    6.6 Update main func

--- socialpoll/api/main.go
+++ socialpoll/api/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"net/http"
+	"time"
 
 	"gopkg.in/mgo.v2"
+	"gopkg.in/tylerb/graceful.v1"
 )
 
 func main() {
-
+	var (
+		addr  = flag.String("addr", ":8080", "エンドポイントのアドレス")
+		mongo = flag.String("mongo", "localhost", "MongoDB のアドレス")
+	)
+	flag.Parse()
+	log.Println("MongoDB に接続します", *mongo)
+	db, err := mgo.Dial(*mongo)
+	if err != nil {
+		log.Fatalln("MongoDB への接続に失敗しました:", err)
+	}
+	defer db.Close()
+	mux := http.NewServeMux()
+	mux.HandlerFunc("/polls/", withCORS(withVars(withData(db, withAPIKey(handlePolls)))))
+	log.Println("Web サーバーを開始します:", *addr)
+	graceful.Run(*addr, 1*time.Second, mux)
+	log.Println("停止します...")
 }
 
 func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
