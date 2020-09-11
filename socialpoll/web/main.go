e47aae04f192cde5e0410c2b8b17a9cf0e899579
Lua00808 <3102oikawa713hayaki@gmail.com>
2020-09-12 03:31:01 +0900

    Add /web/main.go

---
+++ socialpoll/web/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func main() {
+	var addr = flag.String("addr", ":8081", "web サイトのアドレス")
+	flag.Parse()
+	mux := http.NewServeMux()
+	mux.Handle("/", http.StripPrefix("/",
+		http.FileServer(http.Dir("public"))))
+	log.Println("Web サイトのアドレス:", *addr)
+	http.ListenAndServe(*addr, mux)
+}
