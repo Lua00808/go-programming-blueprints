0535b775c60ca6abebc8cc40c201223f82223c12
Lua00808 <3102oikawa713hayaki@gmail.com>
2020-09-16 23:06:17 +0900

    7.3.2.1 Add test

--- chapter7/meander/cost_level_test.go
+++ chapter7/meander/cost_level_test.go
@@ -15,3 +15,11 @@ func TestCostValues(t *testing.T) {
 	is.Equal(int(meander.Cost4), 4)
 	is.Equal(int(meander.Cost5), 5)
 }
+func TestCostString(t *testing.T) {
+	is := is.New(t)
+	is.Equal(int(meander.Cost1.String()), "$")
+	is.Equal(int(meander.Cost2.String()), "$$") http.ResponseWriter, r *http.Request
+	is.Equal(int(meander.Cost3.String()), "$$$")
+	is.Equal(int(meander.Cost4.String()), "$$$$")
+	is.Equal(int(meander.Cost5.String()), "$$$$$")
+}
