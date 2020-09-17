555c069b7f454994f524a5668b18b9a7ed477661
Lua00808 <3102oikawa713hayaki@gmail.com>
2020-09-17 23:02:45 +0900

    7.3.2.1 Add test

--- chapter7/meander/cost_level.go
+++ chapter7/meander/cost_level.go
@@ -1,5 +1,9 @@
 package meander
 
+import (
+	"strings"
+)
+
 type Cost int8
 
 const (
@@ -26,4 +30,24 @@ func (l Cost) String() string {
 		}
 	}
 	return "不正な値です"
+}
+
+func ParseCost(s string) Cost {
+	return costStrings[s]
+}
+
+type CostRange struct {
+	From Cost
+	To   Cost
+}
+
+func (r CostRange) String() string {
+	return r.From.String() + "..." + r.To.String()
+}
+func ParseCostRange(s string) *CostRange {
+	segs := strings.Split(s, "...")
+	return &CostRange{
+		From: ParseCost(segs[0]),
+		To:   ParseCost(segs[1]),
+	}
 }
