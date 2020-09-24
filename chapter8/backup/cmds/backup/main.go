4e731e73e48edc5d3896ea951a0644cf9c46b43d
Lua00808 <3102oikawa713hayaki@gmail.com>
2020-09-24 22:41:53 +0900

    8.3.2.1 Update main.go

--- chapter8/backup/cmds/backup/main.go
+++ chapter8/backup/cmds/backup/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
+	"fmt"
 	"log"
+	"strings"
+
+	"github.com/matryer/filedb"
 )
+
+type path struct {
+	Path string
+	Hash string
+}
+
+func (p path) String() string {
+	return fmt.Printf("%s [%s]", p.Path, p.Hash)
+}
 
 func main() {
 	var fatalErr error
@@ -23,4 +37,32 @@ func main() {
 		fatalErr = errors.New("エラー; コマンドを指定してください")
 		return
 	}
+
+	switch strings.ToLower(args[0]) {
+	case "list":
+		var path path
+		col.ForEach(func(i int, data []byte) bool {
+			err := json.Unmarshal(data, &path)
+			if err != nil {
+				fatalErr = err
+				return true
+			}
+			fmt.Printf("= %s\n", path)
+			return false
+		})
+	case "add":
+	case "remove":
+	}
+}
+
+db, err := filedb.Dial(*dbpath)
+if err != nil {
+	fatalErr = err
+	return
+}
+defer db.Close()
+col, err := db.C("paths")
+if err != nil {
+	fatalErr = err
+	return
 }
