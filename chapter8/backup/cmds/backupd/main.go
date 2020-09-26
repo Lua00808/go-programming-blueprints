c15df93224b78d9120b2d8f6419d9407278fbfad
Lua00808 <3102oikawa713hayaki@gmail.com>
2020-09-27 04:18:11 +0900

    8.4.4 Add func check()

--- chapter8/backup/cmds/backupd/main.go
+++ chapter8/backup/cmds/backupd/main.go
@@ -4,7 +4,12 @@ import (
 	"encoding/json"
 	"errors"
 	"flag"
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-programming-blueprints/chapter8/backup"
 	"github.com/matryer/filedb"
@@ -62,5 +67,50 @@ func main() {
 	if len(m.Paths) < 1 {
 		fatalErr = errors.New("パスがありません。backup ツールを使って追加してください")
 		return
+	}
+
+	check(m, col)
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+Loop:
+	for {
+		select {
+		case <-time.After(time.Duration(*interval) * time.Second):
+			check(m, col)
+		case <-signalChan:
+			fmt.Println()
+			log.Printf("終了します...")
+			break Loop
+		}
+	}
+}
+
+func check(m *backup.Monitor, col *filedb.C) {
+	log.Println("チェックします...")
+	counter, err := m.Now()
+	if err != nil {
+		log.Panicln("バックアップに失敗しました:", err)
+	}
+	if counter > 0 {
+		log.Printf("%d 個のディレクトリをアーカイブしました\n", counter)
+		// ハッシュ値を更新します
+		var path path
+		col.SelectEach(func(_ int, data []byte) (bool, []byte, bool) {
+			if err := json.Unmarshal(data, &path); err != nil {
+				log.Println("JSON データの読み込みに失敗しました"+
+					"次の項目に進みます:", err)
+				return true, data, false
+			}
+			path.Hash, _ = m.Paths[path.Path]
+			newdata, err := json.Marshal(&path)
+			if err != nil {
+				log.Println("JSON データの書き出しに失敗しました。"+
+					"次の項目に進みます:", err)
+				return true, data, false
+			}
+			return true, newdata, false
+		})
+	} else {
+		log.Println("変更はありません")
 	}
 }
